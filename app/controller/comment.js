e7fa87e4ce1df1ac22d76b3d209da7c7ef4da289
sfyr111 <sfyr111@163.com>
2018-04-25 23:48:38 +0800

    添加 comment controller service

---
+++ app/controller/comment.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class CommentController extends Controller {
+  async addCommentsToArticle() {
+    // todo
+  }
+
+  async getCommentsFromArticleSlug() {
+    // todo
+  }
+
+  async deleteCommentBySlugAndId() {
+    // todo
+  }
+}
+
+module.exports = CommentController;
---
+++ app/service/comment.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class CommentService extends Service {
+  async addCommentsToArticle() {
+    // todo
+  }
+
+  async getCommentsFromArticleSlug() {
+    // todo
+  }
+
+  async deleteCommentBySlugAndId() {
+    // todo
+  }
+}
+
+module.exports = CommentService;
